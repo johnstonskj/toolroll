cf635ba40779dd0ad7f8ae73fdbba1448df37d49
johnstonskj@gmail.com <johnstonskj@gmail.com@4dbfb4d4-fad4-11de-a831-516a40b15b2e>
2011-03-08 15:59:39 +0000

    Added more JUnit tests for Strings, additional validation and JavaDoc

--- org.johnstonshome.utils/src/org/johnstonshome/utils/lang/Strings.java
+++ org.johnstonshome.utils/src/org/johnstonshome/utils/lang/Strings.java
@@ -12,10 +12,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -27,7 +29,11 @@ import java.util.regex.Pattern;
 import org.johnstonshome.utils.fun.Functional;
 
 /**
- * This class contains utility classes 
+ * This class contains common utility functions for dealing with Strings in 
+ * Java. Some of these functions are common to String classes in other 
+ * languages (such as {@link #split(String, String)} or {@link #join(String[], String)})
+ *  whereas others are simply common operations necessary when dealing with 
+ *  Strings.
  * 
  * @author Simon Johnston (simon@johnstonshome.org)
  *
@@ -107,7 +113,7 @@ public class Strings {
 	}
 
 	/**
-	 * Combine the set of strings provided with a copy of the separator
+	 * Combine the list of strings provided with a copy of the separator
 	 * string between them.
 	 * 
 	 * @param strings the list of strings to join
@@ -122,6 +128,29 @@ public class Strings {
 		final int           last    = strings.size() -1;
 		for (int i = 0; i < strings.size(); i++) {
 			builder.append(strings.get(i));
+			if (i < last) {
+				builder.append(sep == null ? EMPTY : sep);
+			}
+		}
+		return builder.toString();
+	}
+	
+	/**
+	 * Combine the array of strings provided with a copy of the separator
+	 * string between them.
+	 * 
+	 * @param strings the array of strings to join
+	 * @param sep the separator string
+	 * @return a combined string 
+	 */
+	public static final String join(final String[] strings, final String sep) {
+		if (strings == null) {
+			return null;
+		}
+		final StringBuilder builder = new StringBuilder();
+		final int           last    = strings.length -1;
+		for (int i = 0; i < strings.length; i++) {
+			builder.append(strings[i]);
 			if (i < last) {
 				builder.append(sep == null ? EMPTY : sep);
 			}
@@ -151,16 +180,40 @@ public class Strings {
 	/**
      * Interpolate a string, that is to extract variable references in
      * a template and replace them from a context. The context in this case
-     * is a Map.
+     * is a Map from String->? where the value's <code>toString()</code> 
+     * method will be used to formulate the variable replacement.
+     * 
+     * The following example shows this in action.
+     * 
+     * <pre>
+     *     Map<String, String> variables = new hashMap<String, String>();
+     *     variables.put("author", "My Name");
+     *     variables.put("location", "Here I Am");
+     *     final String template = Strings.fromFile(new File("template.txt"));
+     *     final String result = Strings.interpolate(template, variables, null);
+     *     Strings.toFile(result, new File("my-file.txt"));
+     * </pre>
 	 * 
 	 * @param <V> the type of values in the variable map, usually
 	 *           String or Object
-	 * @param template the template string containing variables.
-	 * @param values values that may be inserted into the template
-	 * @param defaultValue a defaultValue if no value is found for a variable
-	 * @return the expanded form of the template
+	 * @param template the template string containing variables. If the 
+	 *           <em>template</em> is <code>null</code> the returned value 
+     *           will be <code>null</code> also. 
+	 * @param values values that may be inserted into the template. If the
+	 *           map is <code>null</code> then all variables will be replaced
+	 *           with <em>defaultValue</em>.
+	 * @param defaultValue a default value to insert into the output if no 
+	 *           value is found within <em>values</em> for a named variable.
+	 *           If <em>defaultValue</em> is <code>null</code> then the 
+	 *           value returned in the output will be of the form "!key-name!"
+	 *           so that the output clearly signals the error to the user.
+	 * @return the expanded form of the template with all variables either 
+	 *           replaced from <em>values</em>, or with <em>defaultValue</em>.
 	 */
     public static <V> String interpolate(final String template, final Map<String, V> values, final String defaultValue) {
+    	if (template == null) {
+    		return null;
+    	}
         final Pattern       VARIABLE = Pattern.compile("\\$\\{([^\\}]+)\\}");
         final Matcher       matcher  = VARIABLE.matcher(template);
         final StringBuilder output   = new StringBuilder();
@@ -171,10 +224,14 @@ public class Strings {
                 output.append(template.substring(start, matcher.start()));
             }
             final String mapKey = matcher.group(1);
-            if (values.containsKey(mapKey)) {
+            if (values != null && values.containsKey(mapKey)) {
                 output.append(values.get(mapKey).toString());
             } else {
-            	output.append(defaultValue);
+            	if (defaultValue != null) {
+            		output.append(defaultValue);
+            	} else {
+            		output.append(String.format("!%s!", mapKey));
+            	}
             }
             start = matcher.end();
         }
@@ -188,7 +245,8 @@ public class Strings {
      * Read a file entirely into a string value, the caller is responsible
      * for ensuring that the file is representable as a string.
      * 
-     * @param file the file to read from
+     * @param file the file to read from; if <em>file</em> is <code>null</code>
+     *           the return will be <code>null</code>.
      * @return the string contents of the file, or <code>null</code> on error.
      */
     public static String fromFile(final File file) {
@@ -200,24 +258,96 @@ public class Strings {
     }
     
     /**
+     * Write the given string to the specified file, this will always 
+     * overwrite the existing contents replacing it with the value of
+     * <em>source</em>.
+     * 
+     * @param source the string to write to the file; if <em>source</em>
+     *          is <code>null</code> then the file will be created 
+     *          empty.
+     * @param file the file descriptor that denotes the file to write to;
+     *          if <em>file</em> is <code>null</code> the function
+     *          returns <code>false</code>.
+     * @return <code>true</code> if the file was written successfully, 
+     *          else <code>false</code>.
+     */
+    public static boolean toFile(final String source, final File file) {
+    	return toFile(source, file, false);
+    }
+
+    /**
+     * Write the given string to the specified file.
+     * 
+     * @param source the string to write to the file; if <em>source</em>
+     *          is <code>null</code> then the file will be created 
+     *          empty.
+     * @param file the file descriptor that denotes the file to write to;
+     *          if <em>file</em> is <code>null</code> the function
+     *          returns <code>false</code>.
+     * @param append if <code>true</code> the content specified will be
+     *          appended to <em>file</em>, else the file will be replaced
+     *          with the content of <em>source</em>.
+     * @return <code>true</code> if the file was written successfully, 
+     *          else <code>false</code>.
+     */
+    public static boolean toFile(final String source, final File file, final boolean append) {
+    	boolean ok = (file != null && file.canWrite());
+    	if (ok) {
+    		Writer writer = null;
+    		try {
+				writer = new FileWriter(file, append);
+				if (source != null) {
+					writer.write(source);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+				ok = false;
+    		} finally {
+    			if (writer != null) {
+    				try {
+						writer.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+    			}
+			}
+    	}
+    	return ok;
+    }
+    
+    /**
      * Read a stream entirely into a string value, the caller is responsible
      * for ensuring that the stream is representable as a string.
      * 
-     * @param stream the stream to read from
+     * Note that this function does not close the {@link InputStream}, this is 
+     * expected to be performed by the caller.
+     * 
+     * @param stream the stream to read from; if <em>stream</em> is 
+     *           <code>null</code>the return will be <code>null</code>.
      * @return the string contents of the stream, or <code>null</code> on error.
      */
     public static String fromInputStream(final InputStream stream) {
+    	if (stream == null) {
+    		return null;
+    	}
         return fromReader(new InputStreamReader(stream));
     }
     
     /**
-     * Read from a <em>reader</em> entirely into a string value, the caller is responsible
-     * for ensuring that the reader is representable as a string.
+     * Read from a <em>reader</em> entirely into a string value, the caller is 
+     * responsible for ensuring that the reader is representable as a string.
      * 
-     * @param reader the reader to read from
+     * Note that this function does not close the {@link Reader}, this is 
+     * expected to be performed by the caller.
+     * 
+     * @param reader the reader to read from; if <em>stream</em> is 
+     *           <code>null</code>the return will be <code>null</code>.
      * @return the string contents of the reader, or <code>null</code> on error.
      */
     public static String fromReader(final Reader reader) {
+    	if (reader == null) {
+    		return null;
+    	}
         final BufferedReader buffered = new BufferedReader(reader);
         final StringBuilder  builder  = new StringBuilder();
         String               line     = null;
